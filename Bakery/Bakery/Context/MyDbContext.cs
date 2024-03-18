1d5efaa78e5765d5fea85430eac182d5e6e0bf6d
Søren Mehlsen <78858933+soerenmehlsen@users.noreply.github.com>
2024-03-18 13:54:19 +0100

    Add-code-OnModelCreating

--- Bakery/Bakery/Context/MyDbContext.cs
+++ Bakery/Bakery/Context/MyDbContext.cs
@@ -21,13 +21,54 @@ public class MyDbContext : DbContext
     internal DbSet<Address> Addresses{ get; set; }
     internal DbSet<Batches> Batches{ get; set; }
     internal DbSet<Ingredients> Ingredients{ get; set; }
-    internal DbSet<BatchIngredient> IngredientBatches{ get; set; }
+    internal DbSet<BatchIngredient> BatchIngredients{ get; set; }
     
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));  
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // BakingGoodBatch
+       modelBuilder.Entity<BakingGoodBatch>()
+           .HasKey(i => new { i.BakingGoodId, i.BatchId });
        
+       modelBuilder.Entity<BakingGoodBatch>()
+           .HasOne(e=>e.BakingGood)
+           .WithMany(e => e.BakingGoodBatches)
+           .HasForeignKey(e=>e.BakingGoodId);
+       
+       modelBuilder.Entity<BakingGoodBatch>()
+           .HasOne(e=>e.Batch)
+           .WithMany(e => e.BakingGoodBatches)
+           .HasForeignKey(e=>e.BatchId);
+       
+       // BatchIngredient
+       modelBuilder.Entity<BatchIngredient>()
+           .HasKey(i => new { i.BatchId, i.IngredientId });
+       
+       modelBuilder.Entity<BatchIngredient>()
+           .HasOne(e=>e.Batch)
+           .WithMany(e => e.BatchIngredients)
+           .HasForeignKey(e=>e.BatchId);
+       
+       modelBuilder.Entity<BatchIngredient>()
+           .HasOne(e=>e.Ingredient)
+           .WithMany(e => e.BatchIngredients)
+           .HasForeignKey(e=>e.IngredientId);
+       
+       // OrderBakingGood
+       modelBuilder.Entity<OrderBakingGood>()
+           .HasKey(i => new { i.OrderId, i.BakingGoodId });
+       
+       modelBuilder.Entity<OrderBakingGood>()
+           .HasOne(e=>e.Order)
+           .WithMany(e => e.OrderBakingGoods)
+           .HasForeignKey(e=>e.OrderId);
+       
+       modelBuilder.Entity<OrderBakingGood>()
+           .HasOne(e=>e.BakingGood)
+           .WithMany(e => e.OrderBakingGoods)
+           .HasForeignKey(e=>e.BakingGoodId);
+
     }
 }
