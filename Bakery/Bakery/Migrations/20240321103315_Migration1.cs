fe5a861086f5b2a0a48e6a2744cef479454a4df0
Jzjepsen <jzjepsen@gmail.com>
2024-03-26 14:42:34 +0100

    adding migration1

---
+++ Bakery/Bakery/Migrations/20240321103315_Migration1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bakery.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Allergens",
+                columns: table => new
+                {
+                    AllergenId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Allergens", x => x.AllergenId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IngredientAllergens",
+                columns: table => new
+                {
+                    IngredientId = table.Column<int>(type: "int", nullable: false),
+                    AllergenId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredientAllergens", x => new { x.IngredientId, x.AllergenId });
+                    table.ForeignKey(
+                        name: "FK_IngredientAllergens_Allergens_AllergenId",
+                        column: x => x.AllergenId,
+                        principalTable: "Allergens",
+                        principalColumn: "AllergenId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IngredientAllergens_Ingredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "IngredientId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientAllergens_AllergenId",
+                table: "IngredientAllergens",
+                column: "AllergenId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IngredientAllergens");
+
+            migrationBuilder.DropTable(
+                name: "Allergens");
+        }
+    }
+}
